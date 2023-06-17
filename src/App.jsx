660f1169655db206521c61a9b6d2242c8d7444ec
sammun1926 <munorusam10@gmail.com>
2023-06-17 05:53:04 +0300

    updated components

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
 import Testimonials from "./components/testimonials/Testimonials";
 import Works from "./components/works/Works";
+import Menu from "./components/menu/Menu";
 import "./app.scss"
 import { useState } from "react";
+
+
+
 function App() {
-  const {menuOpen,setMenuOpen} = useState(false)
+
+ const [menuOpen,setMenuOpen] = useState(false)
   return (
 
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         
         <Intro/>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,11 @@
 import "./Topbar.scss"
 import PersonIcon from '@mui/icons-material/Person';
 import MailIcon from '@mui/icons-material/Mail';
-export default function Topbar({menuOpen, setMenuOpen}) {
+
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar  ">
+    <div className={"topbar " + (menuOpen &&  "active")}>
         <div className="wrapper">
             <div className="left">
                 <a href="#intro" className="logo">Papschulo.</a>
@@ -22,7 +24,7 @@ export default function Topbar({menuOpen, setMenuOpen}) {
             </div>
             
             <div className="right">
-              <div className="hamburger">
+              <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                 <span className="line1"></span>
                 <span className="line2"></span>
                 <span className="line3"></span>
