d745c76ec0acecf19d93991a424503cde8e82b39
sammun1926 <munorusam10@gmail.com>
2023-06-17 03:32:34 +0300

    updated components

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,28 @@
 import "./Topbar.scss"
-
+import PersonIcon from '@mui/icons-material/Person';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 export default function Topbar() {
   return (
     <div className="topbar">
         <div className="wrapper">
             <div className="left">
-                <a href="#intro">sasa</a>
+                <a href="#intro" className="logo">Papschulo.</a>
+                <div className="itemContainer">
+                  <PersonIcon className="icon"/>
+                  <span>+254704223079</span>
+                
+                  
+                </div>
+                <div className="itemContainer">
+               < MailOutlineIcon className="icon"/>
+                  <span>munorusam10@gmail.com</span>
+                
+                  
+                </div>
             </div>
+            
             <div className="right"></div>
+         
 
         </div>
 
