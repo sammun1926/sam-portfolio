a6103077ad9c0a3d416d08f4ebeeeef721553cde
sammun1926 <munorusam10@gmail.com>
2023-06-18 03:12:48 +0300

    updated files

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,19 @@ import "./Intro.scss"
 export default function Intro() {
   return (
     <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/paps.jpeg" alt=""/>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, </h2>
+          <h2>Welcome to the los Porlos Hermanos family</h2>
+          <h1>My Name Is SAM but you can call me PAPS!</h1>
+        </div>
+      </div>
+
         
     </div>
   )
