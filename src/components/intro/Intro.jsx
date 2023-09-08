169359dd6c8548c60ff0f0fab003fe55e0fae0cc
sammun1926 <munorusam10@gmail.com>
2023-09-08 21:07:45 +0300

    updates icons

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,4 +1,7 @@
-import "./Intro.scss"
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram , faLinkedin, } from '@fortawesome/free-brands-svg-icons';
+import "./Intro.scss";
 
 export default function Intro() {
   return (
@@ -11,16 +14,23 @@ export default function Intro() {
       <div className="right">
         <div className="wrapper">
           <h3>Let's Build</h3>
-          <h2>Hi There,I'm Sam Maina </h2>
-          <h1> A <spam>Software Engineer</spam></h1>
+          <h2>Hi There, I'm Sam Maina </h2>
+          <h1> A <span>Software Engineer</span></h1>
           <h3>Based in Kenya</h3>
+          <div className="social-icons">
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faLinkedin} />
+             
+            
+          </div>
         </div>
+        
         <a href="#portfolio">
           <img src="assets/down2.png" alt=""/>
-          </a>
-      </div>
-
-        
+        </a>
+      </div>   
     </div>
   )
 }
